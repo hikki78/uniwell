02b0ae4509b29fd3e00a7c2d64677d8d8cd2e89c
hikki78 <meghadrimukherjee680@gmail.com>
2025-04-27 09:00:46 +0000

    deployment issue

--- lib/api.ts
+++ lib/api.ts
@@ -15,10 +15,7 @@ import {
 import { notFound } from "next/navigation";
 import { ACTIVITY_PER_PAGE } from "./constants";
 
-export const domain =
-  process.env.NODE_ENV !== "production"
-    ? "http://localhost:3000"
-    : "http://localhost:3000";
+export const domain = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
 
 export const getWorkspace = async (workspace_id: string, userId: string) => {
   const res = await fetch(
